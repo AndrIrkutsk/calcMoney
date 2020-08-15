9d747adc6df1a459558f52338222bc1dbd3ce959
AndrIrkutsk <57530249+AndrIrkutsk@users.noreply.github.com>
2020-08-15 18:59:24 +0800

    Add files via upload

---
+++ precache-manifest.fecdda2e6552a3a7223e87312b4a0e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123188ef4c9362dcbb09dec0a78ae1ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b27b99e3bff51a20eb74",
+    "url": "/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "61359d1134d1e6df5d7d",
+    "url": "/static/js/2.9de72480.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9de72480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27b99e3bff51a20eb74",
+    "url": "/static/js/main.7bcdeeb5.chunk.js"
+  },
+  {
+    "revision": "4b1f5a7be84e29d8307f",
+    "url": "/static/js/runtime-main.92a0df90.js"
+  }
+]);
